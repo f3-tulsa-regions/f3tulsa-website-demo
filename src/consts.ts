02d39e48b276c018e5f5070291528d4349bbbcd0
Klint Van Tassel <developer@sprocktech.com>
2025-08-21 15:15:53 -0500

    cleaned up default repo, setup for F3 Tulsa Regions

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "F3 Tulsa Regions";
+export const SITE_DESCRIPTION = "Website for F3 Tulsa Regions, including T-Town and Scissortail.";
